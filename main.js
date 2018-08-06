b8154f71829edee6399fd7ed47305b955d94e383
mattrudin <matthias.rudin@live.com>
2018-08-06 18:12:43 +0200

    main.js updated; padStart not supported

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const add = (a, b) => {
-	//Plan of action
-	// 1. Save length of both strings
-	// 2. Compare both lengths and add zeros with IFELSE
-	// 2.1 If a is shorter than b, pad zeros to a with String.prototype.padStart()
-	// 2.2 If b is shorter than a, pad zeros to b with String.prototype.padStart()
-	// 3. Sum both strings up with magic (FORLOOP?), store it in result
-	// 4. return result
+	
+	const lengthA = a.length;
+	const lengthB = b.length;
+	
+	if(lengthA > lengthB) {
+		b = b.padStart(lengthA, '0')
+	} else {
+		a = a.padStart(lengthB, '0')
+	}
+	// lengthA > lengthB ? b.padStart(lengthA, '0') : a.padStart(lengthB, '0')
+	const reverseA = a.split('').reverse();
+	const reverseB = b.split('').reverse();
+
+	let result = [];
+	let acc = 0;
+	for(let i = 0; i < reverseA.length; i++) {
+		let resultTemp = parseInt(reverseA[i]) + parseInt(reverseB[i]) + acc;
+		if(resultTemp >= 10) {
+			result.push(resultTemp - 10);
+			acc = 1;
+		} else {
+			result.push(resultTemp);
+			acc = 0;
+		}
+	}
+
+	return result = result.reverse().join('');
 }
